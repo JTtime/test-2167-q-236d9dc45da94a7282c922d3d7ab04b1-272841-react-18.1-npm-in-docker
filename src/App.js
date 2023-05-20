4c5673c5ce8e2e95c0e2bd533aa529ced84121bd
Jeevraj Taralkar <JEEVRAJ.VJTI@gmail.com>
2023-05-20 11:33:33 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import LandingPage from "./Components/LandingPage";
+import WeatherForecast from "./Components/WeatherForecast";
+import "./Components/LandingPage.css";
+import { MyContext } from "./Components/MyContext";
+import React, { useState } from "react";
+import TabComponent from "./Components/TabComponent";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+  const [selectedCity, setSelectedCity] = useState({
+    id: 2,
+    name: "Mumbai",
+    backgroundUrl:
+      "https://cdn.pixabay.com/photo/2015/01/23/12/43/gateway-of-india-609076_1280.jpg",
+    categories: null,
+    weather: {
+      temp: 28,
+      main: "Mist",
+      icon: "http://openweathermap.org/img/w/50d.png"
+    }
+  });
+  const [searchedCity, setSearchedCity] = useState("Mumbai");
+  const [placesData, setPlacesData] = useState([{id:1, name:"Mumbai"}]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyContext.Provider
+      value={{
+        data,
+        setData,
+        selectedCity,
+        setSelectedCity,
+        searchedCity,
+        setSearchedCity, placesData, setPlacesData
+      }}
+    >
+      <div className="App">
+        <WeatherForecast />
+
+        <LandingPage />
+        <TabComponent/>
+      </div>
+    </MyContext.Provider>
   );
 }
-
-export default App;
